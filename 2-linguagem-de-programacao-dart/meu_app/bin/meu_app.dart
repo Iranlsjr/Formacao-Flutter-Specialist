0fe49a349195fbda0468aff56f0fb8405458919e
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-11 17:53:20 -0300

    Tipos de dados ee Declaração de Variáveis

--- 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
+++ 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
@@ -3,31 +3,16 @@
 void main(List<String> arguments) {
   int numero1 = 10;
   var numero2 = 11;
-  
-  print("Retorna verdadeiro se e somente se esse inteiro for par");
+
+  print("Retorna verdadeiro se e somente se esse interio for par");
   print(numero1.isEven);
   print(numero2.isEven);
-
-  print("Retorna Verdade se e somente se este inteiro for ímpar");
+  print("------------------------------------------------------");
+  print("Retorna verdadei se é somente se este inteio for ímpar");
   print(numero1.isOdd);
   print(numero2.isOdd);
+  print("------------------------------------------------------");
 
-  print("Retorna se o número é finito");
-  print(numero1.isFinite);
 
-  print("Retorna se o número é infinito");
-  print(double.infinity);
-
-  print(" Retorna se o número não é um número válido");
-  print(numero1.isNaN);
-
-  print("Retorna se o número é negativo");
-  print(numero1.isNegative);
-  print((numero1 * -1).isNegative);
-
-  print("Converte  String para inteiro");
-  print(int.parse("10"));
-  print(int.tryParse("teste"));
-
-  ///print('Hello world: ${meu_app.calculate()}!');
+  //print('Hello world: ${meu_app.calculate()}!');
 }
---
+++ 2-linguagem-de-programacao-dart/meu_app/bin/tipo_dados/tipos.dart
@@ -0,0 +1,36 @@
+//import 'package:meu_app/meu_app.dart' as meu_app;
+
+void main(List<String> arguments) {
+  //String - Texto
+  String texto = "Essa é uma String";
+  print(texto);
+  var texto2 = "Essa é uma String 2";
+  print(texto2);
+  String texto3;
+  texto3  =  "Essa é uma String 3";
+  print(texto3);
+
+  // Inteiro
+  print("--------------------");
+  int numero1 = 1;
+  print(numero1);
+  var numero2 = 2;
+  print(numero2);
+  int numero3;
+  numero3 = 3;
+  print(numero3);
+
+  //lista Tipada
+  print("--------------------");
+  List<String> lista = [];
+  lista.add("A");
+
+  //Lista dinamica
+  print("--------------------");
+  var lista1 = [];
+  lista1.add(1);
+  lista1.add("A");
+  lista1.add(1.2);
+
+  //print('Hello world: ${meu_app.calculate()}!');
+}
