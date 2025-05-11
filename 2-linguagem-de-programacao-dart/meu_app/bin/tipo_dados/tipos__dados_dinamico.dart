991896889fead14b54e2225dc91b2673c59b9a90
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-11 20:43:40 -0300

    Tipos de dados Dinâmico

---
+++ 2-linguagem-de-programacao-dart/meu_app/bin/tipo_dados/tipos__dados_dinamico.dart
@@ -0,0 +1,30 @@
+void main(List<String> arguments) {
+  print("String");
+  dynamic var1 = "ABC";
+  print(var1);
+
+  print("------------------------------------------------------");
+  print("Inteiro");
+  var1 = 10;
+  print(var1);
+
+  print("------------------------------------------------------");
+  print("Double");
+  var1 = 99.54;
+  print(var1);
+
+  print("------------------------------------------------------");
+  print("Boolean");
+  var1 = true;
+  print(var1);
+
+  print("------------------------------------------------------");
+  print("Data");
+  var1 = DateTime(2025, 05, 11);
+  print(var1);
+
+  print("------------------------------------------------------");
+  print("Lista");
+  var1 = [10, true, "A"];
+  print(var1);
+}
