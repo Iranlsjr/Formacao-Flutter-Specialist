35d78cf0bf92f08562cf27324823ba99d51dc5cf
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-11 21:00:47 -0300

    Tipos de dados Datetime

--- 2-linguagem-de-programacao-dart/meu_app/bin/tipo_dados/tipos__dados_dateTime.dart
+++ 2-linguagem-de-programacao-dart/meu_app/bin/tipo_dados/tipos__dados_dateTime.dart
@@ -1,7 +1,7 @@
 void main(List<String> arguments) {
   DateTime data1 = DateTime.now();
   data1 = DateTime.parse("2025-05-11 00:00:00");
-  
+
   print("Parte da data");
   print(data1);
   print(data1.day);
@@ -12,4 +12,22 @@ void main(List<String> arguments) {
   print(data1.second);
 
   print("------------------------------------------------------");
+  print("Dia da seamana");
+  print(data1.weekday);
+
+  print("------------------------------------------------------");
+  print("Soma de datas");
+  print(data1);
+  data1 = data1.add(Duration(days: 1));
+  print(data1.add(Duration(days: 1)));
+  print(data1);
+  print(data1.subtract(Duration(hours: 1)));
+  print(data1);
+
+  print("------------------------------------------------------");
+  print("Comparação de data");
+  var data2 = DateTime.parse("2025-05-11 00:00:00");
+  print(data1.isAfter(data2));
+  print(data1.isBefore(data2));
+  print(data1.compareTo(data2));
 }
