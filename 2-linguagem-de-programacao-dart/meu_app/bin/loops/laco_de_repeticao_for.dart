0a81309760446b930a04e12620a2f316fc16fb91
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-12 19:10:08 -0300

    Estruturas condicionais e de repetição em Dart - Laço de repetição FOR

---
+++ 2-linguagem-de-programacao-dart/meu_app/bin/loops/laco_de_repeticao_for.dart
@@ -0,0 +1,40 @@
+//import 'package:meu_app/meu_app.dart' as meu_app;
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) {
+  var texto = "Meu primeiro bloco for";
+  for (var i = 0; i < texto.length; i++) {
+    print(texto[i]);
+  }
+
+  //lista
+  var listaNomes = ["Iran", "Josy", "Duda"];
+  for (var i = 0; i < listaNomes.length; i++) {
+    print(listaNomes[i]);
+  }
+
+  // Soma número de Zero a cem
+  var resultado = 0;
+  for (var i = 1; i <= 100; i++) {
+    resultado = resultado + i;
+    print("$i - $resultado");
+  }
+
+//Digitando o número
+  resultado = 0;
+  var quantidade = 0;
+  print("Informe a quantidade de números:  ");
+  var line = stdin.readLineSync(encoding: utf8);
+  quantidade = int.parse(line ?? "0");
+
+  for (var i = 0; i < quantidade; i++) {
+    print("Informe o número: ");
+    line = stdin.readLineSync(encoding: utf8);
+    var numero = int.parse(line ?? "0");
+
+    resultado = resultado + numero;
+    print("$i - $resultado");
+  }
+  //print('Hello world: ${meu_app.calculate()}!');
+}
--- 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
+++ 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
@@ -1,9 +1,7 @@
 //import 'package:meu_app/meu_app.dart' as meu_app;
-import 'dart:io';
-import 'dart:convert';
+
 
 void main(List<String> arguments) {
-
 
   //print('Hello world: ${meu_app.calculate()}!');
 }
