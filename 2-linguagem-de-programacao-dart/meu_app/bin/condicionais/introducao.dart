9e7aecfb78431729e3f7f75bd89c2de22f8f5f4c
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-11 23:27:09 -0300

    Estruturas condicionais e de repetição em Dart - Operadores Lógicos

---
+++ 2-linguagem-de-programacao-dart/meu_app/bin/condicionais/introducao.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {
+  var prova1 = 10;
+  var prova2 = 5;
+
+  var media = (prova1 + prova2) / 2;
+
+  print(media);
+
+  //Operadores Lógicos.
+  print(media >= 7);
+  print(media < 7);
+  print(prova1 == prova2);
+  print(prova1 != prova2);
+  print((prova1 >= 7) && (prova2 >= 7));
+  print((prova1 == 10) || (prova2 == 7));
+}
