c96263785a93f6f5363c62e8e98f3882ba582366
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-13 05:55:34 -0300

    Funções  - Introdução

---
+++ 2-linguagem-de-programacao-dart/meu_app/bin/functions/functions.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+void main(List<String> arguments) {
+  printHelloWorld();
+  printName("Iran");
+
+  var number = returnNumber();
+  print(number);
+
+  var resultado = sum(9, 5);
+  print(resultado);
+}
+
+void printHelloWorld() {
+  print("---------------");
+  print("Hello World!");
+}
+
+void printName(String name) {
+  print("---------------");
+  print("My name is: $name.");
+}
+
+int returnNumber() {
+  print("---------------");
+  return 30;
+}
+
+int sum(int number1, int number2) {
+  print("---------------");
+  return number1 + number2;
+}
--- 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
+++ 2-linguagem-de-programacao-dart/meu_app/bin/meu_app.dart
@@ -1,23 +1,6 @@
 //import 'package:meu_app/meu_app.dart' as meu_app;
 
-
-import 'dart:convert';
-import 'dart:io' show stdin;
-
 void main(List<String> arguments) {
-  var opcao = "";
-  var acumulador = 0.0;
 
-  do {
-    print("Digite um número ou 'S' para sair: ");
-    var line = stdin.readLineSync(encoding: utf8);
-    opcao = line ?? "";
-    
-    var numero = double.tryParse(opcao);
-    if (numero != null) {
-      acumulador = acumulador + numero;
-    }
-  } while (opcao != "S");
-  print(acumulador);
   //print('Hello world: ${meu_app.calculate()}!');
 }
