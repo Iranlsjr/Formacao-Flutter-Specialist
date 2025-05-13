dcbb3027c0894dbdbfb1d036234bc6e417724570
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-13 13:59:01 -0300

    Encapsulamento (get e set)

--- 2-linguagem-de-programacao-dart/meu_app_oo/bin/meu_app_oo.dart
+++ 2-linguagem-de-programacao-dart/meu_app_oo/bin/meu_app_oo.dart
@@ -1,4 +1,10 @@
+import 'package:meu_app_oo/classes/pessoa.dart';
+
 void main(List<String> arguments) {
   var p1 = new Pessoa();
-  print(p1);
+  p1.setNome("Iran");
+  p1.setEndereco("Rua 1");
+  
+  print(p1.getNome());
+  print(p1.getEndereco());
 }
--- 2-linguagem-de-programacao-dart/meu_app_oo/lib/classes/Pessoa.dart
+++ 2-linguagem-de-programacao-dart/meu_app_oo/lib/classes/Pessoa.dart
@@ -1,1 +1,22 @@
-class Pessoa {}
+class Pessoa {
+  String _nome = "";
+  String _endereco = "";
+
+  void setNome(String nome) {
+    _nome = nome;
+  }
+
+  //Criando Get
+  String getNome() {
+    return _nome;
+    //return _nome.toUpperCase();
+  }
+
+  void setEndereco(String endereco) {
+    _endereco = endereco;
+  }
+
+  String getEndereco() {
+    return _endereco;
+  }
+}
