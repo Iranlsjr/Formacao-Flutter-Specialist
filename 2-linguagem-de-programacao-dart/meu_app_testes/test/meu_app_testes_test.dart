30e7e5ff9ad96e0b7cf788d621926db9475958f2
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-14 20:08:50 -0300

    Conhecendo Matchers

--- 2-linguagem-de-programacao-dart/meu_app_testes/test/meu_app_testes_test.dart
+++ 2-linguagem-de-programacao-dart/meu_app_testes/test/meu_app_testes_test.dart
@@ -20,7 +20,54 @@ void main() {
     expect(app.calcularDesconto(1000, 15, true), equals(850));
   });
 
-  test('Calcula o valor do produto com desconto Zeradocom porcentagem', () {
-    expect(()=>  app.calcularDesconto(1000, 0, true), throwsA(TypeMatcher<ArgumentError>()));
+  test('Calcula o valor do produto com desconto Zerado com porcentagem', () {
+    expect(
+      () => app.calcularDesconto(1000, 0, true),
+      throwsA(TypeMatcher<ArgumentError>()),
+    );
   });
+
+  // Teste em grupo
+  group("Calcula o valor do produto com desconto: ", () {
+    var valuesToTest = {
+      {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
+      {'valor': 1000, 'desconto': 15, 'percentual': true}: 850,
+    };
+    valuesToTest.forEach((values, expected) {
+      test(' Entrada: $values Esperado: $expected ', () {
+        expect(
+          app.calcularDesconto(
+            double.parse(values["valor"].toString()),
+            double.parse(values["desconto"].toString()),
+            values["percentual"] == true,
+          ),
+          equals(expected),
+        );
+      });
+    });
+  });
+
+  // erros
+
+  group(
+    "Calcula o valor do produto informando valores Zerados DEVE GERAR ERRO: ",
+    () {
+      var valuesToTest = {
+        {'valor': 0, 'desconto': 150, 'percentual': false},
+        {'valor': 1000, 'desconto': 0, 'percentual': true},
+      };
+      for (var values in valuesToTest) {
+        test(' Entrada: $values', () {
+          expect(
+            () => app.calcularDesconto(
+              double.parse(values["valor"].toString()),
+              double.parse(values["desconto"].toString()),
+              values["percentual"] == true,
+            ),
+            throwsA(TypeMatcher<ArgumentError>()),
+          );
+        });
+      }
+    },
+  );
 }
