2bf2219f4dea912fe2a3edcbaadc02c7c85da31c
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-14 07:55:21 -0300

    Evoluindo métodos com TRatamento de Erro

--- 2-linguagem-de-programacao-dart/meu_app_ex/bin/meu_app_ex.dart
+++ 2-linguagem-de-programacao-dart/meu_app_ex/bin/meu_app_ex.dart
@@ -1,17 +1,5 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:meu_app_ex/meu_app_ex.dart' as meu_app_ex;
 
 void main(List<String> arguments) {
-  print("Digite um número: ");
-  var line = stdin.readLineSync(encoding: utf8);
-  try {
-    double numero = double.parse(line ?? "");
-    print(numero);
-  } catch (e) {
-    print("Número Inválido, seu digito foi: '$line'");
-  }finally{
-    print("Executando finally");
-  }
+  meu_app_ex.execute();
 }
--- 2-linguagem-de-programacao-dart/meu_app_ex/lib/meu_app_ex.dart
+++ 2-linguagem-de-programacao-dart/meu_app_ex/lib/meu_app_ex.dart
@@ -1,3 +1,21 @@
-int calculate() {
-  return 6 * 7;
+import 'package:meu_app_ex/models/aluno.dart';
+import 'package:meu_app_ex/models/console_utils.dart';
+
+void execute() {
+  print("Bem vindo ao sistema de notas.");
+  String nome = consoleUtils.lerStringComTexto("Digite o nome do aluno(a)");
+  var aluno = Aluno(nome);
 }
+
+
+ // print("Sistema de Notas: ");
+  
+  //Tratamento de erro
+  //try {
+   // double numero = double.parse(line ?? "");
+   // print(numero);
+  //} catch (e) {
+   // print("Número Inválido, seu digito foi: '$line'");
+  //}finally{
+   // print("Executando finally");
+  //}
