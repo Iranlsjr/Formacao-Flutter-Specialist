84ce8dcb29a70635227e24eda7f6d6e0f9185318
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-14 19:35:41 -0300

    Teste que esperam erros

--- 2-linguagem-de-programacao-dart/meu_app_testes/lib/meu_app_testes.dart
+++ 2-linguagem-de-programacao-dart/meu_app_testes/lib/meu_app_testes.dart
@@ -1,5 +1,11 @@
 double calcularDesconto(double valor, double desconto, bool percentual) {
-  if (percentual) {
+  if (valor <= 0) {
+    throw ArgumentError("O valor do produto não pode ser Zero!");
+  }
+  else if (desconto <= 0) {
+    throw ArgumentError("O valor do desconto não pode ser Zero!");
+  }
+  else if (percentual) {
     return valor - ((valor * desconto) / 100);
   }
   return valor - desconto;
