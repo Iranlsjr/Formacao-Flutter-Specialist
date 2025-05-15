78b777df34f6915caf2087b0b83f5acdd05663e5
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-14 21:30:26 -0300

    Teste com Mock e HTTP

--- 2-linguagem-de-programacao-dart/meu_app_testes/lib/meu_app_testes.dart
+++ 2-linguagem-de-programacao-dart/meu_app_testes/lib/meu_app_testes.dart
@@ -1,20 +1,22 @@
+
+
 double calcularDesconto(double valor, double desconto, bool percentual) {
   if (valor <= 0) {
     throw ArgumentError("O valor do produto não pode ser Zero!");
-  }
-  else if (desconto <= 0) {
+  } else if (desconto <= 0) {
     throw ArgumentError("O valor do desconto não pode ser Zero!");
-  }
-  else if (percentual) {
+  } else if (percentual) {
     return valor - ((valor * desconto) / 100);
   }
   return valor - desconto;
 }
 
-String convertToUpper(String texto){
-  return  texto.toUpperCase();
+String convertToUpper(String texto) {
+  return texto.toUpperCase();
 }
 
-double retornaValor(double valor){
+double retornaValor(double valor) {
   return valor;
 }
+
+
--- 2-linguagem-de-programacao-dart/meu_app_testes/test/meu_app_testes_test.dart
+++ 2-linguagem-de-programacao-dart/meu_app_testes/test/meu_app_testes_test.dart
@@ -1,6 +1,12 @@
+import 'package:meu_app_testes/classes/viacep.dart';
 import 'package:meu_app_testes/meu_app_testes.dart' as app;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'meu_app_testes_test.mocks.dart';
+
+@GenerateMocks([MockViaCEP])
 void main() {
   test('Calcula o valor do produto com desconto sem porcentagem', () {
     expect(app.calcularDesconto(1000, 150, false), equals(850));
@@ -48,7 +54,6 @@ void main() {
   });
 
   // erros
-
   group(
     "Calcula o valor do produto informando valores Zerados DEVE GERAR ERRO: ",
     () {
@@ -71,19 +76,44 @@ void main() {
     },
   );
 
-
-//convertToUpper
+  //convertToUpper
   test('Testar conversão para uppercase', () {
     expect(app.convertToUpper("ola"), equals("OLA"));
   });
-// Converte iginorando case
+  // Converte iginorando case
   test('Testar conversão para uppercase tese 2', () {
     expect(app.convertToUpper("ola"), equalsIgnoringCase("ola"));
   });
 
-
   // Retorna valor
-    test('Valor maior que 50', () {
+  test('Valor maior que 50', () {
     expect(app.retornaValor(50), greaterThan(49));
   });
+
+  // Retorna CEP
+  test('Retornar CEP', () async {
+    MockMockViaCEP mockMockViaCEP = MockMockViaCEP();
+    when(mockMockViaCEP.retornarCEP("79063760")).thenAnswer(
+      (realInvocation) => Future.value({
+        "cep": "79063-760",
+        "logradouro": "Rua Antônio Rodrigues",
+        "complemento": "",
+        "unidade": "",
+        "bairro": "Conjunto Residencial Recanto dos Rouxinóis",
+        "localidade": "Campo Grande",
+        "uf": "MS",
+        "estado": "Mato Grosso do Sul",
+        "regiao": "Centro-Oeste",
+        "ibge": "5002704",
+        "gia": "",
+        "ddd": "67",
+        "siafi": "9051",
+      }),
+    );
+
+    var body = await mockMockViaCEP.retornarCEP("79063760");
+    expect(body["localidade"], equals("Campo Grande"));
+  });
 }
+
+class MockViaCEP extends Mock implements ViaCEP {}
