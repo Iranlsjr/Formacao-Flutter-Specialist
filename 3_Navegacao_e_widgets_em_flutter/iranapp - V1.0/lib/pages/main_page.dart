e006c6f570e613f3614572d7ac152a5f6266cc68
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-21 19:48:59 -0300

    Conhecendo o BottonNavigationBar

--- 3_Navegacao_e_widgets_em_flutter/iranapp - V1.0/lib/pages/main_page.dart
+++ 3_Navegacao_e_widgets_em_flutter/iranapp - V1.0/lib/pages/main_page.dart
@@ -14,67 +14,110 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  PageController controller = PageController(initialPage: 0);
+  int posicaoPagina = 0;
+
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
-      child: Scaffold(appBar: AppBar(title: Text("Main Page")),
-      
-      drawer: Drawer(child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            
-            InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                width: double.infinity,
-                child: Text("Dados Cadastrais")),
-              onTap: (){
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => DadosCadastraisPage(texto: "Dados Cadastrais",  dados: ["Nome", "Endereco"]) ));
-              },),
-            Divider(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Main Page")),
 
-                        InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                width: double.infinity,
-                child: Text("Termos de Uso E Privacidade")),
-              onTap: (){
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => TermosDeUsoEPrivacidade()));
-              },),
-            
-            Divider(),
-            SizedBox(height: 10,),
-                     InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                width: double.infinity,
-                child: Text("Configurações")),
-              onTap: (){
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Configuracoes()));
-              },),
-            Divider(),
-            ],
-        ),
-      ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView(
-              scrollDirection: Axis.vertical,
+        drawer: Drawer(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-              Pagina1Pages(),
-              Pagina2Pages(),
-              Pagina3Pages()
-            ],),
+                InkWell(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: Text("Dados Cadastrais"),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => DadosCadastraisPage(
+                              texto: "Dados Cadastrais",
+                              dados: ["Nome", "Endereco"],
+                            ),
+                      ),
+                    );
+                  },
+                ),
+                Divider(),
+
+                InkWell(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: Text("Termos de Uso E Privacidade"),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TermosDeUsoEPrivacidade(),
+                      ),
+                    );
+                  },
+                ),
+
+                Divider(),
+                SizedBox(height: 10),
+                InkWell(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: Text("Configurações"),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Configuracoes()),
+                    );
+                  },
+                ),
+                Divider(),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: controller,
+                onPageChanged: (value) {
+                  setState(() {
+                    posicaoPagina = value;
+                  });
+                },
+                children: [Pagina1Pages(), Pagina2Pages(), Pagina3Pages()],
+              ),
+            ),
+            BottomNavigationBar(
+              onTap: (value) {
+                controller.jumpToPage(value);
+              },
+              currentIndex: posicaoPagina,
+              items: [
+                BottomNavigationBarItem(label: "Pag1", icon: Icon(Icons.home)),
+                BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.add)),
+                BottomNavigationBarItem(
+                  label: "Pag3",
+                  icon: Icon(Icons.person),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
