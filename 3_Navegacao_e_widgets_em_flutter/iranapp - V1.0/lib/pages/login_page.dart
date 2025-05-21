ada5ba3879b8014ba4db6578801558c8d80c5f89
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-21 13:40:15 -0300

    Conhecendo o TextField

--- 3_Navegacao_e_widgets_em_flutter/iranapp - V1.0/lib/pages/login_page.dart
+++ 3_Navegacao_e_widgets_em_flutter/iranapp - V1.0/lib/pages/login_page.dart
@@ -23,15 +23,26 @@ class _LoginPageState extends State<LoginPage> {
               Row(
                 children: [
                   Expanded(child: Container()),
-                  Expanded(flex: 4, child: Image.network("https://hermes.digitalinnovation.one/assets/diome/logo.png",)),
+                  Expanded(
+                    flex: 4,
+                    child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                    ),
+                  ),
                   Expanded(child: Container()),
-              //Icon(Icons.person, size: 150, color: Colors.white,),
+                  //Icon(Icons.person, size: 150, color: Colors.white,),
                 ],
               ),
               SizedBox(height: 20),
-              Text("Já tem cadastro?", style: TextStyle(color: Colors.white, fontSize: 26),),
+              Text(
+                "Já tem cadastro?",
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
               SizedBox(height: 10),
-              Text("Faça seu login e make the change_", style: TextStyle(color: Colors.white, fontSize: 16),),
+              Text(
+                "Faça seu login e make the change_",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
               //Email
               SizedBox(height: 40),
               Container(
@@ -40,27 +51,60 @@ class _LoginPageState extends State<LoginPage> {
                 //color: Colors.green,
                 height: 30,
                 alignment: Alignment.center,
-                child: Row(
-                  
-                  children: [
-                    Expanded(flex:2, child: Text("Informe seu e-mail: ", style: TextStyle(color: Colors.white, fontSize: 14),)),
-                    Expanded(flex: 3, child: Text("E-mail", style: TextStyle(color: Colors.white, fontSize: 14),)),
-                  ],
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 0),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 63, 133, 26),
+                      ),
+                    ),
+                    hintText: "E-mail",
+                    hintStyle: TextStyle(color: Colors.white),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: const Color.fromARGB(255, 63, 133, 26),
+                    ),
+                  ),
+                  //  children: [
+                  //Expanded(flex:2, child: Text("Informe seu e-mail: ", style: TextStyle(color: Colors.white, fontSize: 14),)),
+                  // Expanded(flex: 3, child: Text("E-mail", style: TextStyle(color: Colors.white, fontSize: 14),)),
+                  // ],
                 ),
               ),
               //Senha
-              SizedBox(height: 10),
+              SizedBox(height: 20),
               Container(
                 width: double.infinity,
                 margin: EdgeInsets.symmetric(horizontal: 30),
                 //color: Colors.green,
                 height: 30,
                 alignment: Alignment.center,
-                child: Row(
-                  children: [
-                    Expanded(flex: 2, child: Text("Informe a Senha:", style: TextStyle(color: Colors.white, fontSize: 14))),
-                    Expanded(flex: 3, child: Text("Senha", style: TextStyle(color: Colors.white, fontSize: 14))),
-                  ],
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 0),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 63, 133, 26),
+                      ),
+                    ),
+                    hintText: "Senha",
+                    hintStyle: TextStyle(color: Colors.white),
+
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: const Color.fromARGB(255, 63, 133, 26),
+                    ),
+
+                    suffixIcon: Icon(
+                      Icons.visibility,
+                      color: const Color.fromARGB(255, 63, 133, 26),
+                    ),
+                  ),
+                  //children: [
+                  //Expanded(flex: 2, child: Text("Informe a Senha:", style: TextStyle(color: Colors.white, fontSize: 14))),
+                  //Expanded(flex: 3, child: Text("Senha", style: TextStyle(color: Colors.white, fontSize: 14))),
+                  //],
                 ),
               ),
 
@@ -69,37 +113,56 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 width: double.infinity,
                 margin: EdgeInsets.symmetric(horizontal: 30),
-              
+
                 height: 40,
                 alignment: Alignment.center,
-                child: 
-                 SizedBox(
+                child: SizedBox(
                   width: double.infinity,
-                   child: TextButton(onPressed: (){}, 
-                   style: ButtonStyle(backgroundColor: WidgetStateProperty.all(const Color.fromARGB(255, 63, 133, 26)),
-                   ),
-                   child: Text("ENTRAR", style: TextStyle(color: Colors.white, fontSize: 20),)),
-                 ),
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        const Color.fromARGB(255, 63, 133, 26),
+                      ),
+                    ),
+                    child: Text(
+                      "ENTRAR",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
 
-//                child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 20),),
+                //                child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 20),),
               ),
               SizedBox(height: 20),
-              //  ESQUECI A SENHA 
+              //  ESQUECI A SENHA
               Expanded(child: Container()),
-               Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                height: 30,
-                alignment: Alignment.center,
-                child: Text("Esqueci Minha Senha", style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255), fontSize: 16),), 
-              ),
-
-              //CADASTRA-SE
-              SizedBox(height: 10,),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 30),
                 height: 30,
                 alignment: Alignment.center,
-                child: Text("Cadastre-se", style: TextStyle(color: const Color.fromARGB(255, 63, 133, 26), fontSize: 25),), 
+                child: Text(
+                  "Esqueci Minha Senha",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+
+              //CADASTRA-SE
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 63, 133, 26),
+                    fontSize: 25,
+                  ),
+                ),
               ),
               SizedBox(height: 80),
             ],
